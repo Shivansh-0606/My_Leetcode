67221d5d8d9e5da14d8cd3cec516c8b10afcad44
Shivansh Jhalani <shivanshjhalani123@gmail.com>
2026-08-17 16:41:01 +0530

    Implement unique paths using multiple approaches
    Added three methods to calculate unique paths: tabulation, memoization, and recursion.

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,3 +1,4 @@
+Using Tabulation:-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
@@ -20,3 +21,45 @@ class Solution:
                     dp[i][j] = up + left
         
         return dp[m-1][n-1]
+
+using Memoizaion:-
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp = [[-1] * n for _ in range(m)]
+
+        def calculate_path(i:int , j:int) -> int:
+            if i == 0 and j == 0:
+                return 1
+            if i < 0 or j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            dp[i][j] = calculate_path(i-1,j) + calculate_path(i , j -1)
+
+            return dp[i][j]
+        
+        return calculate_path(m-1,n-1)
+
+Using Recurison:-
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def F(i,j):
+            if i == 0 and j == 0:
+                return 1
+            
+            if i < 0 or j < 0:
+                return 0
+            
+            left = F(i-1,j)
+            up = F(i , j-1)
+
+            return left + up
+        
+        
+        return F(m-1,n-1)
+
+        
