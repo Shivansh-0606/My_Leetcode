54bf401c1156d73f1619591ffecbdd673efa2ca8
Shivansh Jhalani <shivanshjhalani123@gmail.com>
2026-08-19 17:04:06 +0530

    Time: 15 ms (99.77%) | Memory: 29.5 MB (97.25%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,65 +1,27 @@
-class Node:
-    def __init__(self,key:int = 0,val:int = 0):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
-
+from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-
-        self.head = Node()
-        self.tail = Node()
-
-        self.head.next = self.tail
-        self.tail.prev = self.head
-    
-    def remove_from_cache(self,node:Node)-> None:
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-    
-    def move_to_front(self,node:Node)-> None:
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
-        node = self.cache[key]
-
-        self.remove_from_cache(node)
-        self.move_to_front(node)
-
-        return node.val
+        self.cache.move_to_end(key)
+        return self.cache[key]
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self.remove_from_cache(node)
-            self.move_to_front(node)
+            self.cache.move_to_end(key)
         
-        else:
-            new_node = Node(key,value)
-            self.cache[key] = new_node
-            self.move_to_front(new_node)
+        self.cache[key] = value
 
-            if len(self.cache) > self.capacity:
-                lru_node = self.tail.prev
-                self.remove_from_cache(lru_node)
-                del self.cache[lru_node.key]
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
 
-
-
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
